2ad39d295a58667784dca6aef06bfc7bc9251c9e
Suharta Banerjee <suharta_banerjee@Suharta-ka-MacBook-Air.local>
2026-09-09 21:40:55 +0530

    Fixed worker service bug

--- services/webhook/worker.py
+++ services/webhook/worker.py
@@ -11,19 +11,26 @@ app.conf.task_routes = {
 
 
 @app.task(name="analyze_pr")
-def analyze_pr(pr_full_name, pr_number, head_sha, installation_id, pull_request_id):
+def analyze_pr(
+    pr_full_name,
+    pr_number,
+    head_sha,
+    installation_id,
+    pull_request_id,
+):
     with httpx.Client() as client:
         response = client.post(
             "http://orchestrator:8002/analyze",
             json={
-                "pr_full_name": pr_full_name,
+                "pr_id": str(pull_request_id),
                 "pr_number": pr_number,
+                "repo_full_name": pr_full_name,
                 "head_sha": head_sha,
                 "installation_id": installation_id,
-                "pull_request_id": pull_request_id
             },
-            timeout=120
+            timeout=120,
         )
+        response.raise_for_status()
 
 
 @app.task(name="trigger_learning")
